6552f553561aec39d86cb58530927b4ad3b88f40
Tiwari <AnandKumar.Tiwari@gartner.com>
2021-06-20 19:18:33 +0530

    Added no task found component

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,5 +1,6 @@
 import React, {useContext} from 'react';
 import {TaskListContext} from '../context/TaskListContext';
+import NoTaskFound from './NoTaskFound';
 import Task from './Task';
 
 const TaskList = () => {
@@ -7,10 +8,12 @@ const TaskList = () => {
     return (
         <div>
             <ul className="list">
-                {
+                { tasks && tasks.length > 0 ?
                     tasks.map((task) => {
                         return <Task task={task} key={task.id}/>
                     })
+                    :
+                    <NoTaskFound></NoTaskFound>
                 }
             </ul>
         </div>
